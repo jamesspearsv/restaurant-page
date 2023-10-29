d4a63a7dfb114e1f80382c0cc2968c50980b80c8
James Spears <jamesspearsv@gmail.com>
2023-10-29 10:33:22 -0400

    Removed hero-content and updated hero style

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,21 +4,16 @@ import "./home.css";
 export default function createHome() {
   // Create page skeleton from utilities
   const main = createSkeleton();
-  main.classList.add("home-section");
 
   const hero = document.createElement("div");
   hero.classList.add("hero");
   main.appendChild(hero);
 
-  const heroContent = document.createElement("div");
-  heroContent.classList.add("hero-content");
-  hero.appendChild(heroContent);
-
   const heading = document.createElement("h1");
   heading.textContent = "Welcome to the Diner!";
-  heroContent.appendChild(heading);
+  hero.appendChild(heading);
 
   const subtitle = document.createElement("p");
   subtitle.textContent = "The #1 spot to eat in WV!";
-  heroContent.appendChild(subtitle);
+  hero.appendChild(subtitle);
 }
